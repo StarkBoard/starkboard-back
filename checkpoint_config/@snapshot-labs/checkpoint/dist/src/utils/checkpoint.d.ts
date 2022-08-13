63980981bf3e2a972f141738fcec15f64029d866
0xExoMonk <0xexomonk@gmail.com>
2022-08-13 20:47:13 +0200

    cleaning checpoint

--- checkpoint_config/@snapshot-labs/checkpoint/dist/src/utils/checkpoint.d.ts
+++
@@ -1,2 +0,0 @@
-import { CheckpointConfig } from '../types';
-export declare const getContractsFromConfig: (config: CheckpointConfig) => string[];
--- checkpoint_config/@snapshot-labs/checkpoint/dist/src/utils/checkpoint.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getContractsFromConfig = void 0;
-const getContractsFromConfig = (config) => {
-    return (config.sources || []).map(source => source.contract);
-};
-exports.getContractsFromConfig = getContractsFromConfig;
