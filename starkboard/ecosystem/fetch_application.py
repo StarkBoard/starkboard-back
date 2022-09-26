e49b973932894a16081c65d10d9c1c76d6f7c97c
Exo <0xexomonk@gmail.com>
2022-09-26 16:56:31 +0200

    Init pipeline OffChain metrics

--- starkboard/ecosystem/fetch_application.py
+++ starkboard/ecosystem/fetch_application.py
@@ -1,5 +1,6 @@
 import os
 import json
+from starkboard.utils import Requester
 
 
 def update_core_ecosystem(db):
@@ -30,3 +31,37 @@ def get_core_ecosystem(db):
     res = db.get_ecosystem_offchain()
     return res
 
+def get_starknet_db_ecosystem():
+    headers = {'Content-Type': 'application/json'}
+    page = 0
+    starknet_db_res = Requester(os.environ.get('STARKNET_ECOSYSTEM_DB_URL')).get(url=f"projects?page={page}&size=100", headers=headers).json()
+    list_applications = starknet_db_res.get('content')
+    while not starknet_db_res.get('last'):
+        page += 1
+        starknet_db_res = Requester(os.environ.get('STARKNET_ECOSYSTEM_DB_URL')).get(url=f"projects?page={page}&size=100", headers=headers).json()
+        list_applications += starknet_db_res.get('content')
+    count_projects = len(list_applications)
+    return list_applications, count_projects
+
+   
+def normalize_list_applications(list_applications):
+    final_apps = []
+    for app in list_applications:
+        final_app = {}
+        final_app['id'] = app.get('id')
+        final_app['application'] = app.get('name')
+        final_app['application_short'] = app.get('short_name')
+        final_app['tags'] = json.dumps(app.get('tags'))
+        final_app['discord'] = app.get('network').get('discord')
+        final_app['website'] = app.get('network').get('website')
+        final_app['twitter'] = app.get('network').get('twitter')
+        final_app['medium'] = app.get('network').get('medium')
+        final_app['github'] = app.get('network').get('github')
+        final_app['isLive'] = app.get('isLive')
+        final_app['isTestnetLive'] = app.get('isTestnetLive')
+        final_app['description'] = app.get('description')
+        final_app['profile_picture_url'] = app.get('network').get('twitterImage')
+        final_app['banner_picture_url'] = app.get('network').get('twitterBanner')
+        final_app['countFollowers'] = app.get('countFollowers')
+        final_apps.append(final_app)
+    return final_apps
